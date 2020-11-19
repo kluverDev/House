bc893dab4dfb11a2a9299f31466753105958b85d
wizzy <kluverdev@gmail.com>
2020-11-19 08:12:01 -0400

    creating our apollo client

--- tinyhouse_v1/clientt/src/index.tsx
+++ tinyhouse_v1/clientt/src/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
 import { Listings } from "./sections";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-
-render(<Listings title= "Listings component" />, document.getElementById("root"));
-
+const client = new ApolloClient({
+    uri: "/api"
+  });
+  
+  render(
+    <ApolloProvider client={client}>
+      <Listings title="TinyHouse Listings" />
+    </ApolloProvider>,
+    document.getElementById("root")
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
