93ebbd0232a316a68c5ee7a5823d61bee0228b0f
wizzy <kluverdev@gmail.com>
2020-11-24 22:21:44 -0400

    database document structure

--- tinyhouse_v1/server/src/lib/types.ts
+++ tinyhouse_v1/server/src/lib/types.ts
@@ -1,19 +1,57 @@
 import { Collection, ObjectId } from "mongodb";
 
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE",
+}
+
 export interface Listing {
   _id: ObjectId;
-  
+  title: string;
+  description: string;
+  image: string;
+  host: string;
+  type: ListingType;
+  address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
 }
 export interface User {
   _id: ObjectId;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
+}
+//`token` field to store the user's login session token
+//walletid is optional and refers to a users payment details
+interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+
+interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
 }
 export interface Booking {
   _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
 
 export interface Database {
-    listings: Collection<Listing>;
-    users: Collection<User>;
-    bookings: Collection<Booking>;
-
-  }
+  listings: Collection<Listing>;
+  users: Collection<User>;
+  bookings: Collection<Booking>;
+}
